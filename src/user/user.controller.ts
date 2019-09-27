98fd01837df2bfbd75da60acd617358d383d0b62
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-09-27 02:11:35 -0300

    Ajustes nos servicos e controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,35 +1,23 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { User } from 'src/models/user';
 import { UserInputDTO } from './dto/user-input';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
 
+  constructor(private readonly userService: UserService){}
+
   @Get()
-  findAll(): User[] {
-    const user: User = {
-      id: '123',
-      email: 'van@cmail.com',
-      name: 'Vanessa',
-      phone: '1234567890',
-      password: '123',
-      username: 'van',
-      avatar_url: 'https://avatars1.githubusercontent.com/u/3089882?s=460&v=4'
-    }
-    return [user]
+  findAll(): Promise<User[]> {
+    return this.userService
+               .findAll()
+               .then(userList => { return userList})
   }
 
   @Post()
-  create(@Body() userInput: UserInputDTO): User {
-    const userCreated: User = {
-      username: userInput.username,
-      password: userInput.password,
-      phone: userInput.phone,
-      avatar_url: userInput.avatar_url,
-      name: userInput.name,
-      email: `${userInput.username}@cmail.com`,
-      id: '8319827638'
-    }
-    return userCreated;
+  create(@Body() userInput): Promise<User> {
+    return this.userService.create(userInput)
   }
+  
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { UserInputDTO } from './dto/user-input';
 
 @Injectable()
 export class UserService {
@@ -10,7 +11,15 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) { }
 
-  findAll(): Promise<User[]> {
-    return this.userRepository.find();
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
   }
+
+  async create(userInput: UserInputDTO) {
+
+    const userEntity = { ...userInput, ...{ email: `${userInput.username}@cmail.br`}}
+
+    return await this.userRepository.create(userEntity)
+  }
+
 }
